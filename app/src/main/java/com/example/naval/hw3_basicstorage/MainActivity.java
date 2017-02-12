bb73bbd9b47dd18698f33c84db1b112dd324d1af
Naval Kumar <navalk@uw.edu>
2017-02-12 00:25:09 -0800

    Initial commit

---
+++ app/src/main/java/com/example/naval/hw3_basicstorage/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.naval.hw3_basicstorage;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void goToSettings(View view) {
+        Intent intent = new Intent(this, saveSettings.class);
+        startActivity(intent);
+    }
+
+}
